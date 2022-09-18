2b8d8e0e706817f264436ad9ba5631158a8a4978
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-18 18:16:19 -0400

    added Gallery component and added props to Nav element

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,26 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props
 
-    const categories = [
-        {
-          name: "commercial",
-          description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ]
-
-      function categorySelected(name) {
-        console.log(`${name} clicked`)
-      }
-
-
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name)
+    }, [currentCategory])
     return (
     <header>
         <h2>
@@ -40,8 +30,8 @@ function Nav() {
                     <span>Contact</span>
                 </li>
                 {categories.map((category) => (
-                    <li className="mx-1" key={category.name}>
-                        <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+                    <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+                        <span onClick={() => setCurrentCategory(category)}>{capitalizeFirstLetter(category.name)}</span>
                     </li>
                 ))}
             </ul>
