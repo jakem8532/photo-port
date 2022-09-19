5da2e32cea24d9777244326456bd57b632611ce1
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-19 00:19:22 -0400

    added test for all components

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Nav from '..'
+
+const categories = [
+    { name: 'portraits', description: 'POrtraits of people in my life'}
+]
+
+const mockCurrentCategory = jest.fn()
+const mockSetCurrentCategory = jest.fn()
+
+afterEach(cleanup)
+
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Nav 
+            categories={categories}
+            setCurrentCategories={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />)
+    })
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav 
+            categories={categories}
+            setCurrentCategories={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+})
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        const { getByLabelText } = render(<Nav
+            categories={categories}
+            setCurrentCategories={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+         />)
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(getByLabelText('camera')).toHaveTextContent('📸')
+    })
+})
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav
+            categories={categories}
+            setCurrentCategories={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+         />)
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!')
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(getByTestId('about')).toHaveTextContent('About')
+    })
+})
+
